b88861a274fd9e6bb0f72faa2e7c8cdafab06b0c
scurrie <99137578+stephencurrie@users.noreply.github.com>
2022-03-30 22:34:54 -0400

    added editable textarea tags to html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,11 @@
 
 var time = moment().format('dddd, MMM Do YYYY hh:mm A');
 $("#current").text(time);
+
+
+// check time, if time, certain class
+
+// js query to click into a field (form?)
+
+// js query to write to local storage
+
