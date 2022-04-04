0d68c751ac8a19f49e7d6ce5e48475e4104cc153
scurrie <99137578+stephencurrie@users.noreply.github.com>
2022-04-04 18:37:07 -0400

    got backgrounds to work by using this variable

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,61 +1,54 @@
+// Displays time on top of page
+
 var time = moment().format("dddd, MMM Do YYYY hh:mm A");
 $("#current").text(time);
 
 // Check what's in local storage and display on page (Surely there is a way to build a for loop and a function to build these dynamically)
 
-if ("hour9" in localStorage) {
-  text = localStorage.getItem("hour9");
-  $("#hour9").children().eq(1).text(text);
-}
-if ("hour10" in localStorage) {
-  text = localStorage.getItem("hour10");
-  $("#hour10").children().eq(1).text(text);
-}
-if ("hour11" in localStorage) {
-  text = localStorage.getItem("hour11");
-  $("#hour11").children().eq(1).text(text);
-}
-if ("hour12" in localStorage) {
-    text = localStorage.getItem("hour12");
-    $("#hour12").children().eq(1).text(text);
-} 
-if ("hour1" in localStorage) {
-    text = localStorage.getItem("hour1");
-    $("#hour1").children().eq(1).text(text);
-}
-if ("hour2" in localStorage) {
-    text = localStorage.getItem("hour2");
-    $("#hour2").children().eq(1).text(text);
-} 
-if ("hour3" in localStorage) {
-    text = localStorage.getItem("hour3");
-    $("#hour3").children().eq(1).text(text);
-} 
-if ("hour4" in localStorage) {
-    text = localStorage.getItem("hour3");
-    $("#hour4").children().eq(1).text(text);
-} 
-if ("hour5" in localStorage) {
-    text = localStorage.getItem("hour5");
-    $("#hour5").children().eq(1).text(text);
-}  
-if ("hour6" in localStorage) {
-    text = localStorage.getItem("hour6");
-    $("#hour6").children().eq(1).text(text);
-} 
+for (var i=1; i<17; i++) {
 
-// Found this code online which was very helpful to troubleshoot
-// var i;
+if ("hour" + i in localStorage) {
+  text = localStorage.getItem("hour" + i);
+  $("#hour" + i).children().eq(1).text(text);
+}
 
-// for (i = 0; i < localStorage.length; i++) {
-//   console.log(
-//     localStorage.key(i) + "=[" + localStorage.getItem(localStorage.key(i)) + "]"
-//   );
+ }
+// if ("hour10" in localStorage) {
+//   text = localStorage.getItem("hour10");
+//   $("#hour10").children().eq(1).text(text);
 // }
-
-// check time, if time, certain class.  Git all the times and put into an array.  check current time, loop through array.  if, then, set class
-
-// js query to get data in the text area
+// if ("hour11" in localStorage) {
+//   text = localStorage.getItem("hour11");
+//   $("#hour11").children().eq(1).text(text);
+// }
+// if ("hour12" in localStorage) {
+//   text = localStorage.getItem("hour12");
+//   $("#hour12").children().eq(1).text(text);
+// }
+// if ("hour1" in localStorage) {
+//   text = localStorage.getItem("hour1");
+//   $("#hour1").children().eq(1).text(text);
+// }
+// if ("hour2" in localStorage) {
+//   text = localStorage.getItem("hour2");
+//   $("#hour2").children().eq(1).text(text);
+// }
+// if ("hour3" in localStorage) {
+//   text = localStorage.getItem("hour3");
+//   $("#hour3").children().eq(1).text(text);
+// }
+// if ("hour4" in localStorage) {
+//   text = localStorage.getItem("hour4");
+//   $("#hour4").children().eq(1).text(text);
+// }
+// if ("hour5" in localStorage) {
+//   text = localStorage.getItem("hour5");
+//   $("#hour5").children().eq(1).text(text);
+// }
+// if ("hour6" in localStorage) {
+//   text = localStorage.getItem("hour6");
+//   $("#hour6").children().eq(1).text(text);
+// }
 
 $(document).ready(function () {
   //This function called when the button is clicked
@@ -72,3 +65,54 @@ $(document).ready(function () {
     localStorage.setItem(hour, txt);
   });
 });
+
+//Change textarea background color based on time
+var checkTime = function () {
+  // Shows time as an intiger
+  var currentTime = parseInt(moment().format("H"));
+  console.log(currentTime);
+
+  //   function setBackground(){
+  // $(document).ready(function(){
+  // $(".text").each(function(){
+  //     var iD = $(".text").attr("id");
+  //     console.log(iD);
+  //     if (iD === currentTime) {
+  //  $(".text").removeClass("timepast timepresent timefuture");
+  //  $(".text").addClass("timepresent");}
+  //     });
+  // });
+  // }
+
+  $(".text").each(function () {
+    var iD = parseInt( $(this).attr("id"));
+    if (iD === currentTime) {
+      // alert("current")
+      $(this)
+        .removeClass("timepast timepresent timefuture")
+        .addClass("timepresent");
+    } else {
+      if (iD < currentTime) {
+        // alert("past")
+        $(this)
+          .removeClass("timepast timepresent timefuture")
+          .addClass("timepast");
+      } else {
+        // alert("future")
+        $(this)
+          .removeClass("timepast timepresent timefuture")
+          .addClass("timefuture");
+      }
+    }
+  });
+
+  // for (i = 0; i < 11; i++) {
+  //   var iD = $(".text")[i].id;
+
+  //   console.log(iD);
+  // }
+  // };
+
+  // checkTime every 5 minutes
+};
+setInterval(checkTime(), 1000 * 60 * 5);
